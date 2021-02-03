0e8616913f5ae472b653873640b0cb471aac1742
Josiah Witt <josiah@jwitt.me>
2021-02-02 22:18:44 -0500

    Bump version to v0.1.2

--- cmd/lambgo/main.go
+++ cmd/lambgo/main.go
@@ -15,7 +15,7 @@ import (
 //nolint:gochecknoglobals // Allows injecting the version
 // Version of the CLI.
 // Should be tied to the release version.
-var Version = "0.1.0"
+var Version = "0.1.2"
 
 func main() {
 	app := cmd.App{
