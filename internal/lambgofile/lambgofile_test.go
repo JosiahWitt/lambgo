33b47b9ac4914e62381e9a2e08de0f621c9c627a
Josiah Witt <josiah@jwitt.me>
2025-10-31 19:45:36 -0400

    Use mvdan.cc/sh instead of google/shlex for build flag parsing

--- internal/lambgofile/lambgofile_test.go
+++ internal/lambgofile/lambgofile_test.go
@@ -45,7 +45,9 @@ func TestLoadConfig(t *testing.T) {
 
 	table := []struct {
 		Name string
-		PWD  string
+
+		PWD     string
+		EnvVars map[string]string
 
 		ExpectedConfig *lambgofile.Config
 		ExpectedError  error
@@ -56,7 +58,9 @@ func TestLoadConfig(t *testing.T) {
 	}{
 		{
 			Name: "with valid config in current directory",
-			PWD:  "/my/app",
+
+			PWD: "/my/app",
+
 			ExpectedConfig: &lambgofile.Config{
 				RootPath:     "/my/app",
 				ModulePath:   "github.com/my/app",
@@ -74,7 +78,9 @@ func TestLoadConfig(t *testing.T) {
 
 		{
 			Name: "with valid config in parent directory",
-			PWD:  "/my/app/some/nested/pkg",
+
+			PWD: "/my/app/some/nested/pkg",
+
 			ExpectedConfig: &lambgofile.Config{
 				RootPath:     "/my/app",
 				ModulePath:   "github.com/my/app",
@@ -92,7 +98,9 @@ func TestLoadConfig(t *testing.T) {
 
 		{
 			Name: "with valid config including zippedFileName",
-			PWD:  "/my/app",
+
+			PWD: "/my/app",
+
 			ExpectedConfig: &lambgofile.Config{
 				RootPath:       "/my/app",
 				ModulePath:     "github.com/my/app",
@@ -113,7 +121,9 @@ zippedFileName: some-name
 
 		{
 			Name: "with valid config including buildFlags",
-			PWD:  "/my/app",
+
+			PWD: "/my/app",
+
 			ExpectedConfig: &lambgofile.Config{
 				RootPath:      "/my/app",
 				ModulePath:    "github.com/my/app",
@@ -134,8 +144,440 @@ buildFlags: -foo -bar "baz qux"
 		},
 
 		{
+			Name: "with buildFlags containing empty quoted strings",
+
+			PWD: "/my/app",
+
+			ExpectedConfig: &lambgofile.Config{
+				RootPath:      "/my/app",
+				ModulePath:    "github.com/my/app",
+				RawBuildFlags: `-flag ""`,
+				BuildFlags:    []string{"-flag", ""},
+				OutDirectory:  "tmp",
+				Goos:          "linux",
+				Goarch:        "amd64",
+				BuildPaths:    []string{"lambdas/hello_world"},
+			},
+
+			SetupMocks: setupMapFS(mapFS{
+				"my/app/go.mod": defaultGoModFile,
+				"my/app/.lambgo.yml": `
+buildFlags: -flag ""
+` + lambgofile.ExampleFile,
+			}),
+		},
+
+		{
+			Name: "with buildFlags containing single quotes",
+
+			PWD: "/my/app",
+
+			ExpectedConfig: &lambgofile.Config{
+				RootPath:      "/my/app",
+				ModulePath:    "github.com/my/app",
+				RawBuildFlags: `-flag 'value with spaces'`,
+				BuildFlags:    []string{"-flag", "value with spaces"},
+				OutDirectory:  "tmp",
+				Goos:          "linux",
+				Goarch:        "amd64",
+				BuildPaths:    []string{"lambdas/hello_world"},
+			},
+
+			SetupMocks: setupMapFS(mapFS{
+				"my/app/go.mod": defaultGoModFile,
+				"my/app/.lambgo.yml": `
+buildFlags: -flag 'value with spaces'
+` + lambgofile.ExampleFile,
+			}),
+		},
+
+		{
+			Name: "with buildFlags containing escaped quotes",
+
+			PWD: "/my/app",
+
+			ExpectedConfig: &lambgofile.Config{
+				RootPath:      "/my/app",
+				ModulePath:    "github.com/my/app",
+				RawBuildFlags: `-flag "value with \"nested\" quotes"`,
+				BuildFlags:    []string{"-flag", `value with "nested" quotes`},
+				OutDirectory:  "tmp",
+				Goos:          "linux",
+				Goarch:        "amd64",
+				BuildPaths:    []string{"lambdas/hello_world"},
+			},
+
+			SetupMocks: setupMapFS(mapFS{
+				"my/app/go.mod": defaultGoModFile,
+				"my/app/.lambgo.yml": `
+buildFlags: -flag "value with \"nested\" quotes"
+` + lambgofile.ExampleFile,
+			}),
+		},
+
+		{
+			Name: "with buildFlags containing multiple spaces",
+
+			PWD: "/my/app",
+
+			ExpectedConfig: &lambgofile.Config{
+				RootPath:      "/my/app",
+				ModulePath:    "github.com/my/app",
+				RawBuildFlags: `-flag    value    -other`,
+				BuildFlags:    []string{"-flag", "value", "-other"},
+				OutDirectory:  "tmp",
+				Goos:          "linux",
+				Goarch:        "amd64",
+				BuildPaths:    []string{"lambdas/hello_world"},
+			},
+
+			SetupMocks: setupMapFS(mapFS{
+				"my/app/go.mod": defaultGoModFile,
+				"my/app/.lambgo.yml": `
+buildFlags: -flag    value    -other
+` + lambgofile.ExampleFile,
+			}),
+		},
+
+		{
+			Name: "with buildFlags containing special characters",
+
+			PWD: "/my/app",
+
+			ExpectedConfig: &lambgofile.Config{
+				RootPath:      "/my/app",
+				ModulePath:    "github.com/my/app",
+				RawBuildFlags: `-ldflags="-X main.version=1.0.0" -tags=prod,dev`,
+				BuildFlags:    []string{"-ldflags=-X main.version=1.0.0", "-tags=prod,dev"},
+				OutDirectory:  "tmp",
+				Goos:          "linux",
+				Goarch:        "amd64",
+				BuildPaths:    []string{"lambdas/hello_world"},
+			},
+
+			SetupMocks: setupMapFS(mapFS{
+				"my/app/go.mod": defaultGoModFile,
+				"my/app/.lambgo.yml": `
+buildFlags: -ldflags="-X main.version=1.0.0" -tags=prod,dev
+` + lambgofile.ExampleFile,
+			}),
+		},
+
+		{
+			Name: "with buildFlags containing mixed quote styles",
+
+			PWD: "/my/app",
+
+			ExpectedConfig: &lambgofile.Config{
+				RootPath:      "/my/app",
+				ModulePath:    "github.com/my/app",
+				RawBuildFlags: `-flag1 "double quotes" -flag2 'single quotes'`,
+				BuildFlags:    []string{"-flag1", "double quotes", "-flag2", "single quotes"},
+				OutDirectory:  "tmp",
+				Goos:          "linux",
+				Goarch:        "amd64",
+				BuildPaths:    []string{"lambdas/hello_world"},
+			},
+
+			SetupMocks: setupMapFS(mapFS{
+				"my/app/go.mod": defaultGoModFile,
+				"my/app/.lambgo.yml": `
+buildFlags: -flag1 "double quotes" -flag2 'single quotes'
+` + lambgofile.ExampleFile,
+			}),
+		},
+
+		{
+			Name: "with buildFlags containing undefined environment variables",
+
+			PWD: "/my/app",
+
+			ExpectedConfig: &lambgofile.Config{
+				RootPath:      "/my/app",
+				ModulePath:    "github.com/my/app",
+				RawBuildFlags: `-ldflags "-X main.version=$UNDEFINED_VAR" -tags $ALSO_UNDEFINED`,
+				BuildFlags:    []string{"-ldflags", "-X main.version=", "-tags"},
+				OutDirectory:  "tmp",
+				Goos:          "linux",
+				Goarch:        "amd64",
+				BuildPaths:    []string{"lambdas/hello_world"},
+			},
+
+			SetupMocks: setupMapFS(mapFS{
+				"my/app/go.mod": defaultGoModFile,
+				"my/app/.lambgo.yml": `
+buildFlags: -ldflags "-X main.version=$UNDEFINED_VAR" -tags $ALSO_UNDEFINED
+` + lambgofile.ExampleFile,
+			}),
+		},
+
+		{
+			Name: "with buildFlags containing escaped dollar signs",
+
+			PWD: "/my/app",
+
+			ExpectedConfig: &lambgofile.Config{
+				RootPath:      "/my/app",
+				ModulePath:    "github.com/my/app",
+				RawBuildFlags: `-ldflags "-X main.version=\$VERSION" -tags \$BUILD_TAGS`,
+				BuildFlags:    []string{"-ldflags", "-X main.version=$VERSION", "-tags", "$BUILD_TAGS"},
+				OutDirectory:  "tmp",
+				Goos:          "linux",
+				Goarch:        "amd64",
+				BuildPaths:    []string{"lambdas/hello_world"},
+			},
+
+			SetupMocks: setupMapFS(mapFS{
+				"my/app/go.mod": defaultGoModFile,
+				"my/app/.lambgo.yml": `
+buildFlags: -ldflags "-X main.version=\$VERSION" -tags \$BUILD_TAGS
+` + lambgofile.ExampleFile,
+			}),
+		},
+
+		{
+			Name: "with buildFlags containing trailing and leading whitespace",
+
+			PWD: "/my/app",
+
+			ExpectedConfig: &lambgofile.Config{
+				RootPath:      "/my/app",
+				ModulePath:    "github.com/my/app",
+				RawBuildFlags: `-flag value`,
+				BuildFlags:    []string{"-flag", "value"},
+				OutDirectory:  "tmp",
+				Goos:          "linux",
+				Goarch:        "amd64",
+				BuildPaths:    []string{"lambdas/hello_world"},
+			},
+
+			SetupMocks: setupMapFS(mapFS{
+				"my/app/go.mod": defaultGoModFile,
+				"my/app/.lambgo.yml": `
+buildFlags:   -flag value
+` + lambgofile.ExampleFile,
+			}),
+		},
+
+		{
+			Name: "with buildFlags containing backslash escapes",
+
+			PWD: "/my/app",
+
+			ExpectedConfig: &lambgofile.Config{
+				RootPath:      "/my/app",
+				ModulePath:    "github.com/my/app",
+				RawBuildFlags: `-flag value\ with\ spaces`,
+				BuildFlags:    []string{"-flag", "value with spaces"},
+				OutDirectory:  "tmp",
+				Goos:          "linux",
+				Goarch:        "amd64",
+				BuildPaths:    []string{"lambdas/hello_world"},
+			},
+
+			SetupMocks: setupMapFS(mapFS{
+				"my/app/go.mod": defaultGoModFile,
+				"my/app/.lambgo.yml": `
+buildFlags: -flag value\ with\ spaces
+` + lambgofile.ExampleFile,
+			}),
+		},
+
+		{
+			Name: "with complex real-world buildFlags",
+
+			PWD: "/my/app",
+
+			ExpectedConfig: &lambgofile.Config{
+				RootPath:      "/my/app",
+				ModulePath:    "github.com/my/app",
+				RawBuildFlags: `-tags netgo,osusergo -ldflags="-s -w -X main.version=v1.2.3"`,
+				BuildFlags:    []string{"-tags", "netgo,osusergo", "-ldflags=-s -w -X main.version=v1.2.3"},
+				OutDirectory:  "tmp",
+				Goos:          "linux",
+				Goarch:        "amd64",
+				BuildPaths:    []string{"lambdas/hello_world"},
+			},
+
+			SetupMocks: setupMapFS(mapFS{
+				"my/app/go.mod": defaultGoModFile,
+				"my/app/.lambgo.yml": `
+buildFlags: -tags netgo,osusergo -ldflags="-s -w -X main.version=v1.2.3"
+` + lambgofile.ExampleFile,
+			}),
+		},
+
+		{
+			Name: "with buildFlags using environment variable expansion",
+
+			PWD: "/my/app",
+			EnvVars: map[string]string{
+				"VERSION": "v1.2.3",
+			},
+
+			ExpectedConfig: &lambgofile.Config{
+				RootPath:      "/my/app",
+				ModulePath:    "github.com/my/app",
+				RawBuildFlags: `-ldflags "-X main.version=$VERSION"`,
+				BuildFlags:    []string{"-ldflags", "-X main.version=v1.2.3"},
+				OutDirectory:  "tmp",
+				Goos:          "linux",
+				Goarch:        "amd64",
+				BuildPaths:    []string{"lambdas/hello_world"},
+			},
+
+			SetupMocks: setupMapFS(mapFS{
+				"my/app/go.mod": defaultGoModFile,
+				"my/app/.lambgo.yml": `
+buildFlags: -ldflags "-X main.version=$VERSION"
+` + lambgofile.ExampleFile,
+			}),
+		},
+
+		{
+			Name: "with buildFlags using multiple environment variables",
+
+			PWD: "/my/app",
+			EnvVars: map[string]string{
+				"VERSION":    "v2.0.0",
+				"GIT_COMMIT": "abc123",
+				"BUILD_TAGS": "prod,netgo",
+			},
+
+			ExpectedConfig: &lambgofile.Config{
+				RootPath:      "/my/app",
+				ModulePath:    "github.com/my/app",
+				RawBuildFlags: `-ldflags "-X main.version=$VERSION -X main.commit=$GIT_COMMIT" -tags $BUILD_TAGS`,
+				BuildFlags:    []string{"-ldflags", "-X main.version=v2.0.0 -X main.commit=abc123", "-tags", "prod,netgo"},
+				OutDirectory:  "tmp",
+				Goos:          "linux",
+				Goarch:        "amd64",
+				BuildPaths:    []string{"lambdas/hello_world"},
+			},
+
+			SetupMocks: setupMapFS(mapFS{
+				"my/app/go.mod": defaultGoModFile,
+				"my/app/.lambgo.yml": `
+buildFlags: -ldflags "-X main.version=$VERSION -X main.commit=$GIT_COMMIT" -tags $BUILD_TAGS
+` + lambgofile.ExampleFile,
+			}),
+		},
+
+		{
+			Name: "with buildFlags using braced variable syntax",
+
+			PWD: "/my/app",
+			EnvVars: map[string]string{
+				"VERSION": "v3.0.0",
+			},
+
+			ExpectedConfig: &lambgofile.Config{
+				RootPath:      "/my/app",
+				ModulePath:    "github.com/my/app",
+				RawBuildFlags: `-ldflags "-X main.version=${VERSION}"`,
+				BuildFlags:    []string{"-ldflags", "-X main.version=v3.0.0"},
+				OutDirectory:  "tmp",
+				Goos:          "linux",
+				Goarch:        "amd64",
+				BuildPaths:    []string{"lambdas/hello_world"},
+			},
+
+			SetupMocks: setupMapFS(mapFS{
+				"my/app/go.mod": defaultGoModFile,
+				"my/app/.lambgo.yml": `
+buildFlags: -ldflags "-X main.version=${VERSION}"
+` + lambgofile.ExampleFile,
+			}),
+		},
+
+		{
+			Name: "with buildFlags using braced variable syntax with fallbacks",
+
+			PWD: "/my/app",
+			EnvVars: map[string]string{
+				"VAR1": "set",
+				"VAR2": "",
+			},
+
+			ExpectedConfig: &lambgofile.Config{
+				RootPath:      "/my/app",
+				ModulePath:    "github.com/my/app",
+				RawBuildFlags: `-flag1=${VAR1:-default1} -flag2=${VAR2:-default2}`,
+				BuildFlags:    []string{"-flag1=set", "-flag2=default2"},
+				OutDirectory:  "tmp",
+				Goos:          "linux",
+				Goarch:        "amd64",
+				BuildPaths:    []string{"lambdas/hello_world"},
+			},
+
+			SetupMocks: setupMapFS(mapFS{
+				"my/app/go.mod": defaultGoModFile,
+				"my/app/.lambgo.yml": `
+buildFlags: -flag1=${VAR1:-default1} -flag2=${VAR2:-default2}
+` + lambgofile.ExampleFile,
+			}),
+		},
+
+		{
+			Name: "with buildFlags mixing literal and expanded variables",
+
+			PWD: "/my/app",
+			EnvVars: map[string]string{
+				"VERSION": "v1.0.0",
+				"LITERAL": "should not expand",
+			},
+
+			ExpectedConfig: &lambgofile.Config{
+				RootPath:      "/my/app",
+				ModulePath:    "github.com/my/app",
+				RawBuildFlags: `-tags netgo -ldflags "-X main.version=$VERSION -X main.literal=\$LITERAL"`,
+				BuildFlags:    []string{"-tags", "netgo", "-ldflags", "-X main.version=v1.0.0 -X main.literal=$LITERAL"},
+				OutDirectory:  "tmp",
+				Goos:          "linux",
+				Goarch:        "amd64",
+				BuildPaths:    []string{"lambdas/hello_world"},
+			},
+
+			SetupMocks: setupMapFS(mapFS{
+				"my/app/go.mod": defaultGoModFile,
+				"my/app/.lambgo.yml": `
+buildFlags: -tags netgo -ldflags "-X main.version=$VERSION -X main.literal=\$LITERAL"
+` + lambgofile.ExampleFile,
+			}),
+		},
+
+		{
+			Name: "with buildFlags using variables with default values when unset",
+
+			PWD: "/my/app",
+			EnvVars: map[string]string{
+				"SET_VAR": "set_value",
+			},
+
+			ExpectedConfig: &lambgofile.Config{
+				RootPath:      "/my/app",
+				ModulePath:    "github.com/my/app",
+				RawBuildFlags: `-ldflags "-X main.set=$SET_VAR -X main.unset=$UNSET_VAR"`,
+				BuildFlags:    []string{"-ldflags", "-X main.set=set_value -X main.unset="},
+				OutDirectory:  "tmp",
+				Goos:          "linux",
+				Goarch:        "amd64",
+				BuildPaths:    []string{"lambdas/hello_world"},
+			},
+
+			SetupMocks: setupMapFS(mapFS{
+				"my/app/go.mod": defaultGoModFile,
+				"my/app/.lambgo.yml": `
+buildFlags: -ldflags "-X main.set=$SET_VAR -X main.unset=$UNSET_VAR"
+` + lambgofile.ExampleFile,
+			}),
+		},
+
+		{
 			Name: "with valid config including goos",
-			PWD:  "/my/app",
+
+			PWD: "/my/app",
+
 			ExpectedConfig: &lambgofile.Config{
 				RootPath:     "/my/app",
 				ModulePath:   "github.com/my/app",
@@ -154,7 +596,9 @@ goos: plan9
 		},
 		{
 			Name: "with valid config including goarch",
-			PWD:  "/my/app",
+
+			PWD: "/my/app",
+
 			ExpectedConfig: &lambgofile.Config{
 				RootPath:     "/my/app",
 				ModulePath:   "github.com/my/app",
@@ -174,7 +618,9 @@ goarch: arm64
 
 		{
 			Name: "with multiple buildPaths",
-			PWD:  "/my/app",
+
+			PWD: "/my/app",
+
 			ExpectedConfig: &lambgofile.Config{
 				RootPath:     "/my/app",
 				ModulePath:   "github.com/my/app",
@@ -197,7 +643,9 @@ buildPaths:
 
 		{
 			Name: "with complex config including all fields and comments",
-			PWD:  "/my/app",
+
+			PWD: "/my/app",
+
 			ExpectedConfig: &lambgofile.Config{
 				RootPath:       "/my/app",
 				ModulePath:     "github.com/my/app",
@@ -235,7 +683,9 @@ buildPaths:
 
 		{
 			Name: "with various whitespace and formatting",
-			PWD:  "/my/app",
+
+			PWD: "/my/app",
+
 			ExpectedConfig: &lambgofile.Config{
 				RootPath:     "/my/app",
 				ModulePath:   "github.com/my/app",
@@ -261,7 +711,8 @@ buildPaths:
 		},
 
 		{
-			Name:          "when missing go.mod file",
+			Name: "when missing go.mod file",
+
 			PWD:           "/my/app",
 			ExpectedError: lambgofile.ErrCannotFindGoModule,
 
@@ -271,7 +722,8 @@ buildPaths:
 		},
 
 		{
-			Name:          "when cannot open go.mod file",
+			Name: "when cannot open go.mod file",
+
 			PWD:           "/my/app",
 			ExpectedError: lambgofile.ErrCannotOpenFile,
 
@@ -282,7 +734,8 @@ buildPaths:
 		},
 
 		{
-			Name:          "when cannot parse go.mod file",
+			Name: "when cannot parse go.mod file",
+
 			PWD:           "/my/app",
 			ExpectedError: lambgofile.ErrCannotParseGoModule,
 
@@ -293,7 +746,8 @@ buildPaths:
 		},
 
 		{
-			Name:          "when cannot find .lambgo.yml file",
+			Name: "when cannot find .lambgo.yml file",
+
 			PWD:           "/my/app",
 			ExpectedError: lambgofile.ErrCannotOpenFile,
 
@@ -303,7 +757,8 @@ buildPaths:
 		},
 
 		{
-			Name:          "when cannot open .lambgo.yml file",
+			Name: "when cannot open .lambgo.yml file",
+
 			PWD:           "/my/app",
 			ExpectedError: lambgofile.ErrCannotOpenFile,
 
@@ -314,7 +769,8 @@ buildPaths:
 		},
 
 		{
-			Name:          "when cannot parse .lambgo.yml file",
+			Name: "when cannot parse .lambgo.yml file",
+
 			PWD:           "/my/app",
 			ExpectedError: lambgofile.ErrCannotUnmarshalFile,
 
@@ -325,7 +781,8 @@ buildPaths:
 		},
 
 		{
-			Name:          "when .lambgo.yml file has bad flag syntax",
+			Name: "when .lambgo.yml file has bad flag syntax",
+
 			PWD:           "/my/app",
 			ExpectedError: lambgofile.ErrCannotParseFlags,
 
@@ -338,6 +795,10 @@ buildPaths:
 
 	ensure.RunTableByIndex(table, func(ensure ensuring.E, i int) {
 		entry := table[i]
+
+		for key, value := range entry.EnvVars {
+			ensure.T().Setenv(key, value)
+		}
 
 		config, err := entry.Subject.LoadConfig(entry.PWD)
 		ensure(err).IsError(entry.ExpectedError)
